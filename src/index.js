0b8660f01bc7c3d492652a4bfc868dad4d42794f
LorenaSTN <lorenstn@hotmail.com>
2024-09-06 12:20:51 +0200

    updatin front and back end template

---
+++ src/index.js
@@ -0,0 +1,16 @@
+//Importar los modulos de NPM que necesito
+const express = require("express");
+const cors = require("cors");
+
+//Crear el servidor.
+const server = express();
+
+//Configurar el servidor.
+server.use(cors());
+server.use(express.json({ limit: "25mb" }));
+
+//Arrancar en un puerto:
+const serverPort = 5001;
+server.listen(serverPort, () => {
+  console.log(`Server listening at: http://localhost:${serverPort}`);
+});
