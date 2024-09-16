39563f2dfa8fff40ae8d611766546d936309077b
LorenaSTN <lorenstn@hotmail.com>
2024-09-16 18:44:04 +0200

    links

--- web/src/components/RouteProject.jsx
+++ web/src/components/RouteProject.jsx
@@ -1,51 +1,47 @@
-import { Link } from "react-router-dom";
+function RouteProject(props) {
+  console.log(props.listProjects);
 
-function DetailProject(props) {
   return (
-    <article className="card">
-      <Link className="card__link" to={`/${props.previewProject.name}`}>
+    <section className="preview route">
+      <div
+        className="projectImage"
+        style={{
+          backgroundImage: `url(${props.listProjects.image})`,
+          backgroundRepeat: "repeat",
+        }}
+      >
+        <div className="card__pin"></div>
+      </div>
+      <article className="card">
         <div className="card__pin"></div>
         <div className="card__pin card__pin--right"></div>
         <h2 className="card__projectTitle">
           <span className="card__projectTitle--text">Proyecto personal</span>
         </h2>
-
         <div className="card__autor">
           <div
             className="card__autorPhoto"
             style={{
-              backgroundImage: `url(${
-                props.previewProject.photo || props.imageAuthor
-              })`,
-              repeat: "repeat",
+              backgroundImage: `url(${props.listProjects.photo})`,
+              backgroundRepeat: "repeat",
             }}
           ></div>
-          <p className="card__job">{props.previewProject.job || "Profesión"}</p>
-          <h3 className="card__name">
-            {props.previewProject.author || "Nombre del autor"}
-          </h3>
+          <p className="card__job">{props.listProjects.job}</p>
+          <h3 className="card__name">{props.listProjects.author}</h3>
         </div>
-
         <div className="card__project">
-          <h3 className="card__name-project">
-            {props.previewProject.name || "Nombre del proyecto"}
-          </h3>
-          <p className="card__slogan">
-            {props.previewProject.slogan || "Eslogan del proyecto"}
-          </p>
+          <h3 className="card__name-project">{props.listProjects.name}</h3>
+          <p className="card__slogan">{props.listProjects.slogan}</p>
           <h3 className="card__descriptionTitle">Descripción del proyecto</h3>
-          <p className="card__description">
-            {props.previewProject.desc || props.lorem}
-          </p>
-
+          <p className="card__description">{props.listProjects.desc}</p>
           <div className="card__technicalInfo">
             <p className="card__technologies">
-              {props.previewProject.technologies || "React JS - HTML - CSS"}
+              {props.listProjects.technologies}
             </p>
             <div className="container-icons">
               <a
                 className="icon icon__www"
-                href={props.previewProject.demo}
+                href={props.listProjects.demo}
                 title="Haz click para ver el proyecto online"
                 target="_blank"
               >
@@ -53,7 +49,7 @@ function DetailProject(props) {
               </a>
               <a
                 className="icon icon__github"
-                href={props.previewProject.repo}
+                href={props.listProjects.repo}
                 title="Haz click para ver el código del proyecto"
                 target="_blank"
               >
@@ -62,8 +58,9 @@ function DetailProject(props) {
             </div>
           </div>
         </div>
-      </Link>
-    </article>
+      </article>
+    </section>
   );
 }
-export default DetailProject;
+
+export default RouteProject;
